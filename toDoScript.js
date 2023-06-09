8725e261dbc570ef31eb2ef4ba42888f15ee73d2
Igor Gazinskii <igorgazinskii6@gmail.com>
2023-06-09 13:05:52 +0300

    v0.2 v0.2

--- toDoScript.js
+++ toDoScript.js
@@ -3,16 +3,27 @@ const b = document.querySelector('button');
 
 b.addEventListener("click", check)
 
+document.addEventListener('DOMContentLoaded', showUs)
+
+
+
 function check(){
   const neww = this.previousElementSibling.value.trim();
   if (neww){
     create(neww);
     this.previousElementSibling.value = "";
+    addInLs(neww);
   }
   else{
     alert("alarm empty");
   }
 }
+
+function showUs() {
+  const todo = JSON.parse(localStorage.getItem('tasks'));
+  if (todo){
+todo.forEach(todos => create(todos)) 
+}}
 
 function create(text){
   const li = document.createElement('li')
@@ -22,7 +33,22 @@ function create(text){
   list.append(li);
 }
 
-function del() {
+function del(value) {
   this.removeEventListener("click", del)
-  this.remove()
+  this.remove() 
+  remFromLs(value)
+}
+
+function addInLs(neww) {
+ const todo = JSON.parse(localStorage.getItem('tasks')) || [];
+  localStorage.setItem('tasks', JSON.stringify([...todo, neww]))
+}
+
+function remFromLs(item) {
+  let todo = JSON.parse(localStorage.getItem('tasks'));
+  const targ = item.target.innerText;
+  console.log(item.target.innerText);
+todo.splice(todo.findIndex(n => n === targ), 1)
+  localStorage.setItem('tasks', JSON.stringify(todo))
+
 }
